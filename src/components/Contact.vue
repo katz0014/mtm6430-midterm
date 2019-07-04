3f1fce467f03d7acc41f5b468355a58e779a4485
Daniel Katz <katz0014@algonquinlive.com>
2019-07-03 23:57:50 -0400

    first push to repository

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,5 +1,8 @@
 <template>
   <div>
+    <h2 class="address">{{ addhead }}</h2>
+    <p>{{ address }}</p>
+    <h2 class="hours">{{ hourshead }}</h2>
     <ul>
       <li v-for="(value, key) in hours">{{ key }}: {{ value }}</li>
     </ul>
@@ -10,10 +13,15 @@ export default {
   data: function() {
     return {
       hours: {
-        Monday: "8 AM - 10 PM",
-        Tuesday: "8 AM - 10 PM", 
-        Wednesday: "8 AM" - 10 PM"
+        Monday: "8AM - 6PM",
+        Tuesday: "8AM - 6PM",
+        Wednesday: "8AM - 6PM",
+        Thursday: "8AM - 6PM", 
+        Friday: ""
       }
+      address: "1435 Woodroffe Avenue, Ottawa, Ontario. K2G 1V8, 613-7274723",
+      addhead: "Address", 
+      hourshead: "Hours of Operation"
     };
   }
 };
